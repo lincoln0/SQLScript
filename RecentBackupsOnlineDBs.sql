fcecc7902258986a28cc0d2bbd9aaf4f526d61a7
lincoln0 <88443776+lincoln0@users.noreply.github.com>
2022-02-23 08:50:27 -0500

    Add files via upload

---
+++ RecentBackupsOnlineDBs.sql
@@ -0,0 +1,39 @@
+
+ ;with cte as (
+	select database_name
+		 , type
+		 , max(backup_start_date) LatestBackup
+	  from msdb.dbo.backupset
+	 where database_name not in ( 'master', 'msdb', 'model', 'tempdb' )
+		   and backup_finish_date > dateadd(y, -7, sysdatetime())
+		   and type = 'D'
+  group by database_name, type  
+			)
+
+	, cte2 as (
+
+    select database_name
+		 , type
+		 , max(backup_start_date) LatestBackup
+	  from msdb.dbo.backupset
+	 where database_name not in ( 'master', 'msdb', 'model', 'tempdb' )
+		   and backup_finish_date > dateadd(day, -10, sysdatetime())
+		   and type = 'L'
+  group by database_name, type   
+			)
+
+	select a.name 
+		 , a.recovery_model_desc
+		 , a.state_desc
+		 , b.type
+		 , b.LatestBackup D_LatestBackup
+		 , c.type
+		 , c.LatestBackup L_LatestBackup
+	  from sys.databases a
+ left join cte b
+		on a.name = b.database_name
+ left join cte2 c
+		on a.name = c.database_name
+	 where a.database_id > 4
+	   and a.state_desc = 'ONLINE'
+	 order by 2 desc
